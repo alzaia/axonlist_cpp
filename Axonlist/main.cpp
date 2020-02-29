b20ac2eb6057dd83601e4b07e840d939c174705e
Aldo Zaimi <aldo.zaimi@polymtl.ca>
2020-02-29 15:12:42 -0500

    adding init files

---
+++ Axonlist/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Axonlist
+//
+//  Created by Rudina Kaprata on 2020-02-29.
+//  Copyright © 2020 AZ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
