43a48b81ac53f949e0fd7ec23d45ddb5a5093d8d
Aldo Zaimi <aldo.zaimi@polymtl.ca>
2020-02-29 15:19:40 -0500

    added initial Axon class def

---
+++ Axonlist/Axon.cpp
@@ -0,0 +1,15 @@
+//
+//  Axon.cpp
+//  Axonlist
+//
+//  Created by Rudina Kaprata on 2020-02-29.
+//  Copyright © 2020 AZ. All rights reserved.
+//
+
+#include "Axon.h"
+
+
+
+
+
+
---
+++ Axonlist/Axon.h
@@ -0,0 +1,48 @@
+//
+//  Axon.hpp
+//  Axonlist
+//
+//  Created by Rudina Kaprata on 2020-02-29.
+//  Copyright © 2020 AZ. All rights reserved.
+//
+
+#ifndef Axon_h
+#define Axon_h
+
+#include <stdio.h>
+
+
+
+
+class Axon
+{
+public:
+    Axon(double centroid_x = 0.0, double centroid_y = 0.0);
+    ~Axon();
+    
+    double get_centroid_x() const;
+    double get_centroid_y() const;
+    
+    void set_centroid_x(double cx);
+    void set_centroid_y(double cy);
+    
+    void print_axon() const;
+    
+    
+private:
+    double centroid_x_;
+    double centroid_y_;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* Axon_h */
